021ca075a037a1982177eb03aae9b349355e616a
JasmineGreenTea <91932332+Cloris-Dong@users.noreply.github.com>
2022-10-20 00:13:41 -0500

    Revert "new project"
    This reverts commit e944d319e273c4040493597c266e642cb7dadbf8.

--- js/Mathflower.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-  let cnv = createCanvas(innerWidth, innerHeight);
-}
-
-
-function windowResized() {
-   resizeCanvas(windowWidth, windowHeight);
-  }
-  //this is to readjust the window when refresh is not pressed;
